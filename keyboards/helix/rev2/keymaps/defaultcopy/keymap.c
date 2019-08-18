dd857f8bb62a980e3ae3887a8c47d89e06e8ffa2
shima28 <shima.w28@gmail.com>
2019-08-18 17:23:43 +0900

    small change

--- keyboards/helix/rev2/keymaps/defaultcopy/keymap.c
+++ keyboards/helix/rev2/keymaps/defaultcopy/keymap.c
@@ -93,16 +93,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       ),
 
   [_RAISE] = LAYOUT( \
-      KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,     KC_F11, \
-      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_UNDS, KC_PLUS, KC_LCBR, JP_RCBR, KC_DEL , \
-      _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC,    KC_BSLS, \
+      KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, \
+      KC_TILD,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_PIPE, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_DEL , \
+      _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
       _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  _______, _______, KC_F12,  _______, _______, KC_PGDN, KC_PGUP, _______, \
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU,    KC_MPLY \
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
       ),
 
   [_QWERTY_WINJP_RAISE] = LAYOUT( \
-      _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, \
-      _______, _______, _______, _______, _______, _______,                   _______, JP_UNDS, JP_PLUS, JP_LCBR, JP_RCBR, KC_DEL, \
+      JP_GRV,  _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, \
+      JP_TILD, _______, _______, _______, _______, _______,                   JP_PIPE, JP_UNDS, JP_PLUS, JP_LCBR, JP_RCBR, KC_DEL, \
       _______, _______, _______, _______, _______, _______,                   _______, JP_MINS, JP_EQL,  JP_LBRC, JP_RBRC, JP_BSLS,\
       _______, _______, _______, _______, _______, _______,  _______, _______,_______, _______, _______, _______, _______, _______, \
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______\
@@ -118,7 +118,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_VIMMOVE] =  LAYOUT( \
       _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, \
-      _______, _______, _______, _______, _______, _______,                   _______, _______, TO(_QWERTY),  _______, _______, _______, \
+      _______, _______, _______, _______, _______, _______,                   _______, _______, _______,  _______, _______, _______, \
       _______, _______, _______, _______, _______, _______,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, \
       _______, _______, KC_DEL,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
       ADJUST,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
@@ -127,7 +127,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_WINMOVE] = LAYOUT( \
       KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
       KC_TAB,  KC_Q,    KC_W,    KC_END,  KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_UP,    KC_BSPC, \
-      KC_LCTL, KC_HOME, KC_S,    KC_DEL,  KC_RGHT, KC_G,                               KC_BSPC, KC_J,    KC_K,    KC_L,    LCTL(JP_SCLN), KC_QUOT, \
+      KC_LCTL, KC_HOME, KC_S,    KC_DEL,  KC_RGHT, KC_G,                               KC_BSPC, KC_J,    KC_K,    KC_L,    KANA,   KC_QUOT, \
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_LEFT,        KC_LBRC, KC_RBRC,   KC_DOWN, KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
       KC_LCTL, ADJUST,  KC_LALT, KC_LGUI, EISU,    LSFT_T(KC_SPC), LOWER,   RAISE,     LSFT_T(KC_SPC),   KANA,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
       ),      
