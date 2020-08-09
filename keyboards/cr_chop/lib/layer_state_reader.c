4cf024ad85b496fcfa2ddba83d8d9a8036fd3bc9
sotaro <sotaro@sotaro-mbp.local>
2020-08-09 19:56:37 +0900

    sumitomo

--- keyboards/cr_chop/lib/layer_state_reader.c
+++ keyboards/cr_chop/lib/layer_state_reader.c
@@ -5,10 +5,12 @@
 
 // in the future, should use (1U<<_LAYER_NAME) instead, but needs to be moved to keymap,c
 #define L_BASE 0
-#define L_LOWER 2
-#define L_RAISE 4
-#define L_ADJUST 8
-#define L_ADJUST_TRI 14
+#define L_QWERTY 1
+#define L_IOS 2
+#define L_WIN 4
+#define L_SUMITOMO 8
+#define L_LOWER 32
+
 
 char layer_state_str[24];
 
@@ -18,16 +20,20 @@ const char *read_layer_state(void) {
   case L_BASE:
     snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Default");
     break;
-  case L_RAISE:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Raise");
+  case L_QWERTY:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Qwerty");
+    break;
+  case L_IOS:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: iOS");
+    break;
+  case L_WIN:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Windows");
+    break;
+  case L_SUMITOMO:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Sumitomo");
     break;
   case L_LOWER:
     snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Lower");
-    break;
-  case L_ADJUST:
-  case L_ADJUST_TRI:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Adjust");
-    break;
   default:
     snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Undef-%ld", layer_state);
   }
